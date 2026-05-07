eafbd60a267d9fce3634b68e5f41d5ad71d253e7
Luiz Guilherme <luizguilucena09@gmail.com>
2026-05-07 01:10:52 -0300

    Update Conexao.php

--- class/Conexao.php
+++ class/Conexao.php
@@ -1,1 +1,20 @@
+<?php
 
+session_start();
+
+class Conexao{
+    static $pdo;
+    static function conectar(){
+        try{
+            self::$pdo = new PDO(
+    "mysql:host=;dbname=;charset=utf8",
+    "Usuário do Banco de Dados",
+    "SUA_SENHA_AQUI"
+);
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return self::$pdo;
+        }catch(Exception $e){
+            echo $e;
+        }
+    }
+}
